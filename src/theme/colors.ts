c48f9a8ed976baacb27733e45bff48af71dff1a5
jayshree <solankijayshree117@gmail.com>
2024-03-08 06:28:55 +0530

    added colors and utilites functions

---
+++ src/theme/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  WHITE: '#fff',
+  BLACK: '#000',
+  BLUE: '#1D4F91',
+  GRAY_BORDER: 'rgba(23,18,54,0.3)',
+  GRAY: '#717171',
+  GRAY_BACKGROUNDD: '#D9D9D9',
+  BLACK_SECONDARY: '#1E1E1E',
+  LIGHT_BACKGROUND: '#F1F1F1',
+  GREEN: '#1D9F39',
+  RED: '#E32222',
+};
---
+++ src/utils/utilities.ts
@@ -0,0 +1,25 @@
+import {Dimensions, PixelRatio, Platform} from 'react-native';
+
+const fontScale = PixelRatio.getFontScale();
+export const {width, height} = Dimensions.get('window');
+export const getFontSize = (size: number) => size / fontScale;
+export const getKey = () => Math.floor(Math.random() * 100000).toString(); //use this only when u need a randome keys on every render
+// based on iPhone 8's scale
+const wscale = width / 360;
+const hscale = height / 720;
+
+export const normalize = (size: number, based = 'width') => {
+  const newSize = based === 'height' ? size * hscale : size * wscale;
+  if (Platform.OS === 'ios') {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  }
+};
+export const getWidth = (size: number) => {
+  return normalize(size, 'width');
+};
+
+export const getHeight = (size: number) => {
+  return normalize(size, 'height');
+};
