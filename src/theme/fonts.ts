41208bc43f7de803504844808ea6d5758e076fa3
jayshree <solankijayshree117@gmail.com>
2024-04-05 00:40:12 +0530

    added fonts for android and ios

---
+++ src/theme/fonts.ts
@@ -0,0 +1,8 @@
+export const fonts = {
+  REGULAR: 'Poppins-Regular',
+  BOLD: 'Poppins-Bold',
+  SEMIBOLD: 'Poppins-SemiBold',
+  MEDIUM: 'Poppins-Medium',
+  LIGHT: 'Poppins-Light',
+  ITALIC: 'Poppins-Italic',
+};
