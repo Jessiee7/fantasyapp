704f1b31bd8bf8ab3f58e001621814bbc7fc71c0
jayshree <solankijayshree117@gmail.com>
2024-04-12 23:22:08 +0530

    updated bottom tab navigator

--- src/routes/AppNavigator.tsx
+++ src/routes/AppNavigator.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BottomTab from './BottomTab';
+import {global_styles} from '@/theme/styles.global';
 
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Main" component={BottomTab} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={global_styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Main" component={BottomTab} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 };
 
